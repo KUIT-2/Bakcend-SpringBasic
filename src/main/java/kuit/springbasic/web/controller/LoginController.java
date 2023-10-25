e7e970ed0c3b3bcd8254f6a822bf8afd537ec8a8
Han Ju Kim <hjkim2902@gmail.com>
2023-10-25 21:56:47 +0900

    feat : Spring Controller 등록

--- src/main/java/kuit/springbasic/web/controller/LoginController.java
+++ src/main/java/kuit/springbasic/web/controller/LoginController.java
@@ -1,15 +1,45 @@
 package kuit.springbasic.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.web.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+
+import static kuit.springbasic.web.util.UserSessionUtils.USER_SESSION_KEY;
+
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("/user/loginForm")
+    public String showLoginForm() {
+        log.info("HomeController.showLoginForm");
+        return "/user/login";
+    }
 
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("/user/loginFailed")
+    public String showLoginFailed() {
+        log.info("HomeController.showLoginFailed");
+        return "/user/loginFailed";
+    }
 
     /**
      * TODO: login
@@ -18,9 +48,81 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
+    //    @RequestMapping("/user/login")
+    public String loginV1(@RequestParam("userId") String userId, @RequestParam("password") String password,
+                          HttpServletRequest request) throws SQLException {
+        log.info("LoginController.loginV1");
+
+        User loggedInUser = new User(userId, password);
+        User user = memoryUserRepository.findByUserId(userId);
+
+        if (user != null && user.equals(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute(USER_SESSION_KEY, user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    //    @RequestMapping("/user/login")
+    public String loginV2(@RequestParam String userId, @RequestParam String password,
+                          HttpServletRequest request) throws SQLException {
+        log.info("LoginController.loginV2");
+
+        User loggedInUser = new User(userId, password);
+        User user = memoryUserRepository.findByUserId(userId);
+
+        if (user != null && user.equals(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute(USER_SESSION_KEY, user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    //    @RequestMapping("/user/login")
+    public String loginV3(String userId, String password,
+                          HttpServletRequest request) throws SQLException {
+        log.info("LoginController.loginV3");
+
+        User loggedInUser = new User(userId, password);
+        User user = memoryUserRepository.findByUserId(userId);
+
+        if (user != null && user.equals(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute(USER_SESSION_KEY, user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    @RequestMapping("/user/login")
+    public String loginV4(@ModelAttribute User loggedInUser,
+                          HttpServletRequest request) throws SQLException {
+        log.info("LoginController.loginV4");
+
+        User user = memoryUserRepository.findByUserId(loggedInUser.getUserId());
+
+        if (user != null && user.equals(loggedInUser)) {
+            HttpSession session = request.getSession();
+            session.setAttribute(USER_SESSION_KEY, user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
 
     /**
      * TODO: logout
      */
+    @RequestMapping("/user/logout")
+    public String logout(HttpServletRequest request) {
+        log.info("LoginController.logout");
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
 
 }
--- src/main/java/kuit/springbasic/web/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/web/controller/qna/AnswerController.java
@@ -1,7 +1,32 @@
 package kuit.springbasic.web.controller.qna;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.db.MemoryAnswerRepository;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.web.domain.Answer;
+import kuit.springbasic.web.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class AnswerController {
 
+    private final MemoryAnswerRepository memoryAnswerRepository;
+    private final MemoryQuestionRepository memoryQuestionRepository;
 
     /**
      * TODO: addAnswer - @PostMapping
@@ -10,5 +35,71 @@ public class AnswerController {
      * addAnswerV2 : @RequestParam, @ResponseBody
      * addAnswerV3 : @ModelAttribute, @ResponseBody
      */
+    //    @RequestMapping(value = "/api/qna/addAnswer", method = RequestMethod.POST)
+    //    @PostMapping("/api/qna/addAnswer")
+    public void addAnswerV0(@RequestParam int questionId, @RequestParam String writer, @RequestParam String contents,
+                            HttpServletResponse response) throws SQLException, IOException {
+        log.info("AnswerController.addAnswerV0");
+
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = memoryAnswerRepository.insert(answer);
+
+        Question question = memoryQuestionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        memoryQuestionRepository.updateCountOfAnswer(question);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("answer", savedAnswer);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(model));
+    }
+
+    //    @PostMapping("/api/qna/addAnswer")
+    public String addAnswerV1(@RequestParam int questionId, @RequestParam String writer, @RequestParam String contents,
+                              Model model) throws SQLException {
+        log.info("AnswerController.addAnswerV1");
+
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = memoryAnswerRepository.insert(answer);
+
+        Question question = memoryQuestionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        memoryQuestionRepository.updateCountOfAnswer(question);
+
+        model.addAttribute("answer", savedAnswer);
+
+        return "jsonView";
+    }
+
+    @ResponseBody
+//    @PostMapping("/api/qna/addAnswer")
+    public Answer addAnswerV2(@RequestParam int questionId, @RequestParam String writer, @RequestParam String contents) throws SQLException {
+        log.info("AnswerController.addAnswerV2");
+
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = memoryAnswerRepository.insert(answer);
+
+        Question question = memoryQuestionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        memoryQuestionRepository.updateCountOfAnswer(question);
+
+        return savedAnswer;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/qna/addAnswer")
+    public Answer addAnswerV3(@ModelAttribute Answer answer) throws SQLException {
+        log.info("AnswerController.addAnswerV3");
+
+        Answer savedAnswer = memoryAnswerRepository.insert(answer);
+
+        Question question = memoryQuestionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        memoryQuestionRepository.updateCountOfAnswer(question);
+
+        return savedAnswer;
+    }
 
 }
