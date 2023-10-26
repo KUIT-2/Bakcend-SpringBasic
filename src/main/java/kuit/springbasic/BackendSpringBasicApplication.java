3a7f24da03cd6b6e159e86889acfcaa388927606
Han Ju Kim <hjkim2902@gmail.com>
2023-10-26 18:05:36 +0900

    feat : add ServletComponentScan

--- src/main/java/kuit/springbasic/BackendSpringBasicApplication.java
+++ src/main/java/kuit/springbasic/BackendSpringBasicApplication.java
@@ -2,8 +2,10 @@ package kuit.springbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class BackendSpringBasicApplication {
 
     public static void main(String[] args) {
