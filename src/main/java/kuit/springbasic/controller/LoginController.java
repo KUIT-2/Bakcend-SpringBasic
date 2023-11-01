392188b7a0e02ea67c4c6901a4038b6f0694017e
dainn <shon1107@naver.com>
2023-11-01 13:24:45 +0900

    LoginController & User Controller

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,15 +1,44 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class LoginController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showLoginForm
      */
+    @RequestMapping("/user/loginForm")
+    public String showLoginForm(Model model) {
+        log.info("LoginController.showLoginForm");
+
+        return "user/login";
+    }
 
     /**
      * TODO: showLoginFailed
+     *
      */
+
+    @RequestMapping("/user/showLoginFailed")
+    public String showLoginFailed(Model model) {
+        log.info("LoginController.showLoginFailed");
+
+        return "user/loginFailed";
+    }
 
     /**
      * TODO: login
@@ -19,8 +48,30 @@ public class LoginController {
      * loginV4 : @ModelAttribute
      */
 
+    @RequestMapping("/user/login")
+    public String login(HttpSession session, @RequestParam String userId, @RequestParam String password) {
+        log.info("LoginController.login");
+        log.info(userId);
+        User user = memoryUserRepository.findByUserId(userId);
+        if(user.isSameUser(userId, password)){
+            log.info("로그인 성공!");
+            session.setAttribute("user", user);
+            session.setAttribute("userId", userId);
+            return "redirect:/";
+        }
+        return "redirect:/user/showLoginFailed";
+    }
+
     /**
      * TODO: logout
      */
+
+    @RequestMapping("/user/logout")
+    public String logout(HttpSession session) {
+        log.info("LoginController.logout");
+        session.removeAttribute("user");
+
+        return "redirect:/";
+    }
 
 }
--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,30 +1,96 @@
 package kuit.springbasic.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.http.HttpRequest;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class UserController {
 
+
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showUserForm
      */
+    @RequestMapping("/user/form")
+    public String showUserForm() {
+        log.info("UserController.showUserForm");
+
+        return "user/form";
+    }
+
+    //redirect -> url주소
+    // return: jsp파일 경로 아ㅏ마도?
 
     /**
      * TODO: createUser
      * createUserV1 : @RequestParam
      * createUserV2 : @ModelAttribute
      */
+    @RequestMapping("/user/signup")
+    public String createUser(@RequestParam String userId, @RequestParam String password,
+                               @RequestParam String name, @RequestParam String email) {
+        log.info("UserController.createUser");
+        User user = new User(userId, password, name, email);
+        memoryUserRepository.insert(user);
+
+        return "redirect:/";
+    }
+
 
     /**
      * TODO: showUserList
      */
+    @RequestMapping("/user/list")
+    public String showUserList(HttpServletRequest request) {
+        log.info("UserController.showUserList");
+        Collection<User> users = memoryUserRepository.findAll();
+        request.setAttribute("users", users);
+
+        return "user/list";
+    }
 
     /**
      * TODO: showUserUpdateForm
      */
+    @RequestMapping("/user/updateForm")
+    public String showUserUpdateForm(HttpSession session, HttpServletRequest request) {
+        log.info("UserController.showUserUpdateForm");
+        request.setAttribute("user",session.getAttribute("user"));
+
+
+        return "user/updateForm";
+    }
 
     /**
      * TODO: updateUser
      * updateUserV1 : @RequestParam
      * updateUserV2 : @ModelAttribute
      */
+    @RequestMapping("/user/update")
+    public String updateUser(HttpSession session,@RequestParam String password,
+                                     @RequestParam String name, @RequestParam String email) {
+        log.info("UserController.updateUser");
+        // userid는 따로 안받아져서 생기는 문제?
+
+        User user = new User((String) session.getAttribute("userId"), password, name, email);
+        memoryUserRepository.update(user);
+
+        return "redirect:/";
+    }
 
 }
