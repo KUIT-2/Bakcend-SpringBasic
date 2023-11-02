99c22e003aba75d846ed6d12b1961931b6c33e43
jaeuk520 <jaeuk520@naver.com>
2023-11-03 00:15:45 +0900

    feat: Controller 구현 (요구사항 0)

--- src/main/java/kuit/springbasic/controller/HomeController.java
+++ src/main/java/kuit/springbasic/controller/HomeController.java
@@ -1,7 +1,5 @@
 package kuit.springbasic.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import kuit.springbasic.db.MemoryQuestionRepository;
 import kuit.springbasic.domain.Question;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -19,32 +16,6 @@ import java.util.List;
 public class HomeController {
 
     private final MemoryQuestionRepository memoryQuestionRepository;
-
-    @RequestMapping("/homeV1")
-//    @RequestMapping("/")
-    public ModelAndView showHomeV1(HttpServletRequest request, HttpServletResponse response) {
-        log.info("HomeController.homeV1");
-
-        ModelAndView modelAndView = new ModelAndView("home");
-
-        List<Question> questions = memoryQuestionRepository.findAll();
-        modelAndView.addObject("questions", questions);
-
-        return modelAndView;
-    }
-
-    @RequestMapping("/homeV2")
-//    @RequestMapping("/")
-    public ModelAndView showHomeV2() {
-        log.info("HomeController.homeV2");
-
-        ModelAndView modelAndView = new ModelAndView("home");
-
-        List<Question> questions = memoryQuestionRepository.findAll();
-        modelAndView.addObject("questions", questions);
-
-        return modelAndView;
-    }
 
     @RequestMapping("/")
     public String showHomeV3(Model model) {
