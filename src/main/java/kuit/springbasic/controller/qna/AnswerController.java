15da4307456fbb543abd091ada4ce2369ccb65a0
jaeuk520 <jaeuk520@naver.com>
2023-11-03 02:23:45 +0900

    feat: qna Controller 구현

--- src/main/java/kuit/springbasic/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/controller/qna/AnswerController.java
@@ -1,14 +1,29 @@
 package kuit.springbasic.controller.qna;
 
+import kuit.springbasic.db.MemoryAnswerRepository;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/qna")
 public class AnswerController {
+    private final MemoryAnswerRepository answerRepository;
+    private final MemoryQuestionRepository questionRepository;
 
-
-    /**
-     * TODO: addAnswer - @PostMapping
-     * addAnswerV0 : @RequestParam, HttpServletResponse
-     * addAnswerV1 : @RequestParam, Model
-     * addAnswerV2 : @RequestParam, @ResponseBody
-     * addAnswerV3 : @ModelAttribute, @ResponseBody
-     */
+    @RequestMapping("/addAnswer")
+    public Answer addAnswer(@ModelAttribute Answer answer) {
+        log.info("AnswerController.addAnswer");
+        answerRepository.insert(answer);
+        Question question = questionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        questionRepository.updateCountOfAnswer(question);
+        return answer;
+    }
 
 }
