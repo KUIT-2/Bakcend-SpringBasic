3bbb8ae19d8575e571bcaa6aeec73acb4e073e38
encoreJeong <zzaibean@gmail.com>
2023-11-02 11:05:42 +0900

    Feat : 회원가입 화면 띄우는 기능 구현

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,11 +1,27 @@
 package kuit.springbasic.controller;
 
+import kuit.springbasic.db.MemoryUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class UserController {
 
+    private final MemoryUserRepository memoryUserRepository;
 
     /**
      * TODO: showUserForm
      */
+    @RequestMapping("/user/form")
+    public String showForm() {
+        log.info("UserController.showForm");
+        return "/user/form";
+    }
+
 
     /**
      * TODO: createUser
