14313b615efd189f15011c66313e3d82cd7219cf
parkjiwon <ji11won@naver.com>
2023-11-04 23:46:04 +0900

    Feat : QnAController 완

--- src/main/java/kuit/springbasic/controller/qna/QnAController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnAController.java
@@ -1,10 +1,35 @@
 package kuit.springbasic.controller.qna;
 
 
+import kuit.springbasic.db.MemoryAnswerRepository;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.tags.shaded.org.apache.xpath.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class QnAController {
+
+    private final MemoryQuestionRepository memoryQuestionRepository;
+    private final MemoryAnswerRepository memoryAnswerRepository;
+
     /**
      * TODO: showQnA
      */
+    @RequestMapping("/qna/show")
+    public String showQnA(@RequestParam("questionId") String questionId, Model model) {
+        log.info("QnAController.showQnA");
+        int id = Integer.parseInt(questionId);
 
+        model.addAttribute("question", memoryQuestionRepository.findByQuestionId(id));
+        model.addAttribute("answers", memoryAnswerRepository.findAllByQuestionId(id));
+        return "/qna/show";
+    }
 
 }
