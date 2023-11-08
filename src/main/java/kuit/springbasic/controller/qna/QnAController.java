d630ba1e0d91a71c955b4d426d7c32124c134712
DDongYul <comeme0101@naver.com>
2023-11-08 13:58:47 +0900

    feat: 요구사항4,추가 - qna 컨트롤러 구현

--- src/main/java/kuit/springbasic/controller/qna/QnAController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnAController.java
@@ -1,13 +1,13 @@
 package kuit.springbasic.controller.qna;
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import kuit.springbasic.db.MemoryAnswerRepository;
 import kuit.springbasic.db.MemoryQuestionRepository;
 import kuit.springbasic.domain.Answer;
 import kuit.springbasic.domain.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,11 +25,11 @@ public class QnAController {
      * TODO: showQnA
      */
     @GetMapping("/show")
-    public String showQnA(HttpServletRequest request, @RequestParam int questionId) {
+    public String showQnA(Model model, @RequestParam int questionId) {
         Question question = memoryQuestionRepository.findByQuestionId(questionId);
         List<Answer> answers = memoryAnswerRepository.findAllByQuestionId(questionId);
-        request.setAttribute("question", question);
-        request.setAttribute("answers", answers);
+        model.addAttribute("question", question);
+        model.addAttribute("answers", answers);
         return "qna/show";
     }
 }
--- src/main/java/kuit/springbasic/controller/qna/QuestionController.java
+++ src/main/java/kuit/springbasic/controller/qna/QuestionController.java
@@ -1,30 +1,89 @@
 package kuit.springbasic.controller.qna;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.domain.Question;
+import kuit.springbasic.domain.User;
+import kuit.springbasic.util.UserSessionUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/qna")
 public class QuestionController {
 
+    private final MemoryQuestionRepository memoryQuestionRepository;
 
     /**
      * TODO: showQuestionForm
      */
+    @GetMapping("/form")
+    public String showQuestionForm(HttpServletRequest request) {
+        if (UserSessionUtils.isLoggedIn(request.getSession())) {
+            return "/qna/form";
+        }
+        return "redirect:/user/loginForm";
+    }
+
 
     /**
      * TODO: createQuestion
      * createQuestionV1 : @RequestParam
      * createQuestionV2 : @ModelAttribute
      */
+    @PostMapping("/create")
+    public String createQuestion(@ModelAttribute Question question) {
+        memoryQuestionRepository.insert(question);
+        return "redirect:/";
+    }
 
     /**
      * TODO: showUpdateQuestionForm
      * showUpdateQuestionFormV1 : @RequestParam, HttpServletRequest, Model
      * showUpdateQuestionFormV2 : @RequestParam, @SessionAttribute, Model
      */
+//    @GetMapping("/updateForm")
+    public String showUpdateQuestionFormV1(@RequestParam int questionId, HttpServletRequest request, Model model) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            return "redirect:/user/loginForm";
+        }
+        Question question = memoryQuestionRepository.findByQuestionId(questionId);
+        if (question.isSameUser(user)) {
+            model.addAttribute("question",question);
+            return "/qna/updateForm";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/updateForm")
+    public String showUpdateQuestionFormV2(@RequestParam int questionId,
+                                         @SessionAttribute(value = "user", required = false) User user, Model model) {
+        if (user == null) {
+            return "redirect:/user/loginForm";
+        }
+        Question question = memoryQuestionRepository.findByQuestionId(questionId);
+        if (question.isSameUser(user)) {
+            model.addAttribute("question",question);
+            return "/qna/updateForm";
+        }
+        return "redirect:/";
+    }
 
     /**
      * TODO: updateQuestion
      */
-
+    @PostMapping("/update")
+    public String updateQuestion(@RequestParam int questionId, @RequestParam String title, @RequestParam String contents) {
+        Question question = memoryQuestionRepository.findByQuestionId(questionId);
+        question.updateTitleAndContents(title, contents);
+        memoryQuestionRepository.update(question);
+        return "redirect:/";
+    }
 }
