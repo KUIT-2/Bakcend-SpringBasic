981074f018f27f7a4722cfdfbd1c39ca3451eab8
beomjun <bzzun0609@github.com>
2023-11-09 00:54:45 +0900

    feat: AnswerRepository

--- src/main/java/kuit/springbasic/db/MemoryAnswerRepository.java
+++ src/main/java/kuit/springbasic/db/MemoryAnswerRepository.java
@@ -2,6 +2,9 @@ package kuit.springbasic.db;
 
 
 import kuit.springbasic.domain.Answer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -13,37 +16,27 @@ import java.util.Map;
 import java.util.Set;
 
 @Repository
+@RequiredArgsConstructor
 public class MemoryAnswerRepository {
 
-    private Map<String, Answer> answers= new HashMap<>();
-    private  static int PK = 0;
+    private final JdbcTemplate jdbcTemplate;
 
-    public MemoryAnswerRepository() {
-        insert(new Answer(1,"함형주","7호선 타세요"));
-    }
-
-    public int getPK(){
-        return ++PK;
-    }
     public List<Answer> findAllByQuestionId(int id) {
-        ArrayList<Answer> result = new ArrayList<>();
-
-        Set<String> answerKeys = answers.keySet();
-        for (String key : answerKeys) {
-            Answer repoAnswer = answers.get(key);
-            if(repoAnswer.getQuestionId() == id){
-                result.add(repoAnswer);
-            }
-        }
-
-        return result;
+        String sql = "select * from answers where questionId=?";
+        return jdbcTemplate.query(sql, answerRowMapper(), id);
     }
-
 
     public Answer insert(Answer answer) {
-        answer.setAnswerId(getPK());
-        answer.setCreatedDate(Date.valueOf(LocalDate.now()));
-        answers.put(Integer.toString(answer.getAnswerId()),answer);
-        return answer;
+        String sql = "insert into answers (questionId, writer, contents) values (?, ?, ?)";
+        return jdbcTemplate.queryForObject(sql, answerRowMapper(), answer.getQuestionId(),
+                answer.getWriter(), answer.getContents());
+    }
+
+    private RowMapper<Answer> answerRowMapper() {
+        return (rs, rowNum) -> {
+            Answer answer = new Answer(rs.getInt("answerId"), rs.getInt("questionId"),
+                    rs.getString("writer"), rs.getString("contents"), rs.getDate("createdDate"));
+            return answer;
+        };
     }
 }
